c8a66889921a26c2a861bbc3fa303aad3ba19fe3
Angelito Casalla <acasallajr@gmail.com>
2018-09-29 16:44:49 +0800

    commit all files within

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'CS0')}}</title>
+    </head>
+    <body>
+        <h1>YEY!</h1>
+        <p>Please complete </p>
+    </body>
+</html>
